ff7141dd3ea11297582a3b6c98b1d986202a6ff6
pumpkinpies <ziadmhmh@gmail.com>
2023-02-23 12:29:30 +0300

    Added About page and created README file

---
+++ README.md
@@ -0,0 +1,8 @@
+# Screen Lead
+
+An app designed to give you more control over your monitors. Made with Electron.
+
+## TODO
+
+-   [ ] Make this thing work
+-   [ ] Implement POC
--- renderer/index.js
+++ renderer/index.js
@@ -1,7 +1,21 @@
 const mainBtn = document.querySelector('#main-btn');
 const counterEl = document.querySelector('#counter');
+const imgInput = document.querySelector('#img-input');
+const img = document.querySelector('#img-container');
+
 let counter = 0;
 mainBtn.addEventListener('click', () => {
     counter++;
     counterEl.textContent = counter;
+});
+imgInput.addEventListener('change', (e) => {
+    console.log('triggered');
+    const file = e.target.files[0];
+    console.log(file);
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    img.src = `${file.path}`
+    image.onload = function () {
+        img.style.height = `${this.height / this.width * 50}px`;
+    };
 });
